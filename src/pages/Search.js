7ee8f9a4e8aaa4e3fb3e769e5bf01b7a5bce71f6
Victor Murilo <victor2706.vm@gmail.com>
2022-07-01 18:10:00 -0300

    Cumpri o requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,36 @@
 import React from 'react';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      valorInput: '',
+    };
+  }
+
   render() {
+    const { valorInput } = this.state;
+    const lengthMinimo = 2;
     return (
       <div data-testid="page-search">
-        <h1>search</h1>
+        <form>
+          <input
+            onChange={ (e) => {
+              this.setState({ valorInput: e.target.value });
+            } }
+            type="text"
+            placeholder="Nome da banda ou artista"
+            data-testid="search-artist-input"
+          />
+          <button
+            type="submit"
+            disabled={ lengthMinimo > valorInput.length }
+            onClick={ (e) => { e.preventDefault(); } }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
